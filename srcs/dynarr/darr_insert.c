d4792330e7d5b0c56e329c5380b6f53cd1734729
ftymchyn <fazzan.07@gmail.com>
2019-06-06 08:14:49 +0300

    added the logic to insert emty data

--- srcs/dynarr/darr_insert.c
+++ srcs/dynarr/darr_insert.c
@@ -5,17 +5,14 @@ void	darr_insert(t_darr *darr, size_t index, void *data)
 	void	*src;
 	void	*dst;
 
-	if (darr && data)
+	if (darr && index <= darr->size)
 	{
-		if (index <= darr->size)
-		{
-			if (darr->size == darr->capacity)
-				darr_resize(darr, (size_t)((double)darr->capacity * 1.5));
-			src = (void*)darr->data + darr->data_type_size * index;
-			dst = src + darr->data_type_size;
-			ft_memmove(dst, src, darr->data_type_size * (darr->size - index));
-			ft_memcpy(src, data, darr->data_type_size);
-			darr->size++;
-		}
+		if (darr->size == darr->capacity)
+			darr_resize(darr, (size_t)((double)darr->capacity * 1.5));
+		src = (void*)darr->data + darr->data_type_size * index;
+		dst = src + darr->data_type_size;
+		ft_memmove(dst, src, darr->data_type_size * (darr->size - index));
+		ft_memcpy(src, data, darr->data_type_size);
+		darr->size++;
 	}
 }
