2def45490c2bae7b807a8a7856678e4e8d5a9874
ftymchyn <fazzan.07@gmail.com>
2019-07-04 07:54:26 +0300

    NULL checking

--- srcs/dynarr/darr_insert.c
+++ srcs/dynarr/darr_insert.c
@@ -9,14 +9,17 @@ void	darr_insert(t_darr *darr, int index, void *data)
 	{
 		if (darr->size == darr->capacity)
 			darr_resize(darr, (int)((double)darr->capacity * 1.5));
-		src = (void*)darr->data + darr->data_type_size * index;
-		dst = src + darr->data_type_size;
-		if (index < darr->size)
-			ft_memmove(dst, src, darr->data_type_size * (darr->size - index));
-		if (data)
-			ft_memcpy(src, data, darr->data_type_size);
-		else
-			ft_bzero(src, darr->data_type_size);
-		darr->size++;
+		if (darr->size < darr->capacity)
+		{
+			src = (void*)darr->data + darr->data_type_size * index;
+			dst = src + darr->data_type_size;
+			if (index < darr->size)
+				ft_memmove(dst, src, darr->data_type_size * (darr->size - index));
+			if (data)
+				ft_memcpy(src, data, darr->data_type_size);
+			else
+				ft_bzero(src, darr->data_type_size);
+			darr->size++;
+		}
 	}
 }
